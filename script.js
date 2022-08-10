074499a31dce8c85b76903a7c4c1aa32791bf067
Lee Yi Cheng <MelvinLee@MacBook-Pro.local>
2022-08-10 22:36:48 +0800

    Refactored functions to be single responsibility

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const container = document.querySelector('.container');
 
 function createSquare() {
     const square = document.createElement('div');
-    square.classList.add('square')
-    container.appendChild(square);
+    square.classList.add('square')  
+    return square;
 }
 
 function createGrid(number) {
     for (let i = 0; i < number*number ; i++) { 
-    createSquare();
-}
+        container.appendChild(createSquare());
+    }
 }
 
 createGrid(30);
