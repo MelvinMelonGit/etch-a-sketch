7d45b33771fd1a8ad33beb5b8132b067c49b9342
Lee Yi Cheng <MelvinLee@MacBook-Pro.local>
2022-08-11 21:34:36 +0800

    Created prompt to set dynamic grid UI

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-//create 16 squares
+//create a grid of squares
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
-function createSquare() {
-    const square = document.createElement('div');
-    square.classList.add('square')  
-    return square;
+button.addEventListener('click', setNumberOfRows);
+
+//prompt to set dynamic number of squares
+function setNumberOfRows() { 
+    rows = parseInt(prompt("How many rows would you want to grid to be?"));
+    createGrid(rows);
 }
 
 function createGrid(number) {
-    for (let i = 0; i < number*number ; i++) { 
-        container.appendChild(createSquare());
+    container.innerHTML = ''; // clear grid
+    if (number > 100) return alert("Please select a value 100 or less"); // keep rows within 100 or less
+    for (let i = 0; i < number*number; i++) { 
+        container.appendChild(createSquare(number));
     }
 }
 
-createGrid(30);
+function createSquare(number) {
+    const square = document.createElement('div');
+    square.classList.add('square');  
+    square.style.height = `calc(100%/${number})`; // set proportional height to number of squares in grid
+    square.style.width = `calc(100%/${number})`; // set proportional width to number of squares in grid
+    return square;
+}
 
 //squares change colour on hover
 
-//prompt to set dynamic number of squares
 
 //extra credit - random colour change, or adding more black 
